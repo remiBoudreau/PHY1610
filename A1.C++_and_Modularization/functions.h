2318732b2d09c67b0c60ad5ac59315296ecc5ba3
Jean-Michel Remi Boudreau <41983349+remiBoudreau@users.noreply.github.com>
2019-08-28 15:11:56 -0400

    Add files via upload

---
+++ A1.C++_and_Modularization/functions.h
@@ -0,0 +1,17 @@
+/*
+Assignment no.:	1  
+Filename: functions.h
+Author: Jean-Michel Boudreau
+Description: Declares functions for functions in functions.cc
+*/
+
+// Preprocessor Guards
+#ifndef FUNCTIONS_H
+#define FUNCTION_H
+
+// Declare functions
+double f(double time);
+double g(double time);
+
+// Terminate directive ifndef
+#endif
