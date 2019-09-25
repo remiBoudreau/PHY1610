9aac53b0c468408fbd791170819336123d9cbee1
Jean-Michel Remi Boudreau <41983349+remiBoudreau@users.noreply.github.com>
2019-09-25 16:18:45 -0400

    Delete allocation.h

--- Assignmen 6: Profiling & Scaling Analysis/allocation.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALLOCATIONH
-#define ALLOCATIONH
-// memory allocation routines
-int** alloc_array_int(int sizex, int sizey);
-void dealloc_array_int(int** result);
-float** alloc_array_float(int sizex, int sizey);
-void dealloc_array_float(float** result);
-#endif
